d6e5aee207c5b6c019b12ac08e5cdcc351f70c7f
TSpes <spes@talon.one>
2025-12-14 21:27:21 +0100

    layouts

--- Building-modern-web-applications-with-go/basic_web_app/4.styling/pkg/render/render.go
+++ Building-modern-web-applications-with-go/basic_web_app/4.styling/pkg/render/render.go
@@ -8,7 +8,8 @@ import (
 
 // RenderTemplates renders templates using html/template
 func RenderTemplates(w http.ResponseWriter, templ string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + templ) 
+	//we need to add all templates here 
+	parsedTemplate, _ := template.ParseFiles("./templates/" + templ, "./templates/base.layout.tmpl") 
 	err := parsedTemplate.Execute(w, nil) 
 	if err != nil {
 		fmt.Println("Error pparsing templater: ", err)
