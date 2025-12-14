a33c66e1cc7018880fd3790aad09bfc3352de1f9
TSpes <spes@talon.one>
2025-12-14 14:41:36 +0100

    handling errors

--- Building-modern-web-applications-with-go/basic_web_app/2.func_and_handlers /main.go
+++ Building-modern-web-applications-with-go/basic_web_app/2.func_and_handlers /main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -28,10 +29,29 @@ func addValue(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+}
+
+func divideValues(x, y float32)(float32, error){
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil) //we specify what to listen, in this case localhost on port 8080
